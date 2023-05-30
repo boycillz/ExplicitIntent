1b4caa482c578534aeaae973c59330cf08711467
Azis Muhammad Ihsan <azis00028@gmail.com>
2023-05-30 11:25:47 +0700

    Update
    Add move activity

--- app/src/main/java/com/boycillz/myapplication/MainActivity.java
+++ app/src/main/java/com/boycillz/myapplication/MainActivity.java
@@ -3,9 +3,10 @@ package com.boycillz.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +14,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button btnMoveActivity = findViewById(R.id.btn_move_activity);
+        btnMoveActivity.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btn_move_activity);
     }
 }
