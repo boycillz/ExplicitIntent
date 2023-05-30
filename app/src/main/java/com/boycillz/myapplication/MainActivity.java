c671154f380e699b46de7b618f28cfae584992e1
Azis Muhammad Ihsan <azis00028@gmail.com>
2023-05-30 11:15:01 +0700

    Add Button

--- app/src/main/java/com/boycillz/myapplication/MainActivity.java
+++ app/src/main/java/com/boycillz/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.boycillz.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnMoveActivity = findViewById(R.id.btn_move_activity);
     }
 }
