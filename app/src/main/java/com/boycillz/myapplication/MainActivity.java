d800235d5697434fd0216b180db8c6839afd2071
Azis Muhammad Ihsan <azis00028@gmail.com>
2023-05-30 11:37:32 +0700

    update
    connecting 2 activities with intent

--- app/src/main/java/com/boycillz/myapplication/MainActivity.java
+++ app/src/main/java/com/boycillz/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.boycillz.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -20,5 +21,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         if (v.getId() == R.id.btn_move_activity);
+        Intent moveIntent = new Intent(MainActivity.this, MoveActivity.class);
+        startActivity(moveIntent);
     }
 }
--- app/src/main/java/com/boycillz/myapplication/MoveActivity.java
+++ app/src/main/java/com/boycillz/myapplication/MoveActivity.java
@@ -10,5 +10,7 @@ public class MoveActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_move);
+
+
     }
 }
