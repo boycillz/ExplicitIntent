0e97571018f48e8adb78d03b81e2609c4f49561f
Azis M. Ihsan <zaxastertu@gmail.com>
2023-05-31 14:45:45 -0700

    update
    dikejar waktu

---
+++ app/src/main/java/com/boycillz/myapplication/Person.java
@@ -0,0 +1,69 @@
+package com.boycillz.myapplication;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import androidx.annotation.NonNull;
+
+public class Person implements Parcelable {
+
+    private String name;
+    private String email;
+    private String city;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    protected Person(Parcel in){
+        name = in.readString();
+        age = in.readInt();
+        email = in.readString();
+        city = in.readString();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(@NonNull Parcel dest, int flags) {
+        dest.writeString(name);
+        dest.writeString(email);
+        dest.writeString(city);
+        dest.writeInt(age);
+
+    }
+
+    ///public static final Creator
+}
